eae4b29b7734975b7adc5da95436f377ee4205c2
Abdullah Usman <abdusman660@gmail.com>
2025-05-01 19:00:17 +0500

    feat: Basic CRUD

---
+++ simple_crud.py
@@ -0,0 +1,41 @@
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+movies = []
+
+@app.route('/movies', methods = ['POST'])
+def add_movies():
+    data = request.get_json()
+    movies.append(data)
+    return jsonify({"message": "Movie Added Sucessfully", "movie": data}),201
+
+
+@app.route('/movies', methods = ['GET'])
+def get_movies():
+    return jsonify(movies)
+
+@app.route('/movies/<int:index>', methods = ['GET'])
+def get_movie_by_index(index):
+    if 0 <= index < len(movies):
+        return jsonify(movies[index])
+    
+    else:
+        return jsonify({'Error' : 'Movie Not Found'}),404
+    
+
+@app.route('/movies/<int:index>', methods = ['PUT'])
+def update_movie(index):
+
+    if 0 <= index < len(movies):
+       data = request.get_json()
+       movies[index] =data
+       return jsonify({'message': 'Movie Updated Sucessfully'})
+    
+    else:
+        return jsonify({"message': 'can't find new movie"}), 404
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
