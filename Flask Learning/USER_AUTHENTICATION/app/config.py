3090a2d37b89db21c8c3d747954ede1afa2e1185
Abdullah Usman <abdusman660@gmail.com>
2025-05-07 23:27:13 +0500

    feat: model and config added

--- Flask Learning/USER_AUTHENTICATION/app/config.py
+++ Flask Learning/USER_AUTHENTICATION/app/config.py
@@ -1,3 +1,10 @@
 
 import os
 
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
+    SQLALCHMEY_DATABASE_URI = 'sqlite:///db.sqlite3'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    
+    
